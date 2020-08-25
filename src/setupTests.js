b1350c03e7b1de76a1c0563104b55f434c575896
John Rake <john.rake12@gmail.com>
2020-08-24 20:34:42 -0500

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+jest.mock('nanoevents', () => {
+    return {
+        createNanoEvents: () => {
+            return {
+                emit: jest.fn(),
+                on: jest.fn()
+            }
+        }
+    }
+})
